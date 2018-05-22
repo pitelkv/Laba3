1e1c62c159f96e769afff17e004b7fb766b73f91
pitelkv <pitelkv@gmail.com>
2018-05-22 16:35:26 +0500

    Initial commit

---
+++ src/com/java/main.java
@@ -0,0 +1,24 @@
+package com.java;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        laba3 mass = new laba3();
+        System.out.println("№1");
+        mass.mass1();
+
+        System.out.println("№2");
+        mass.massiv2();
+
+        System.out.println("№3");
+        mass.massiv3();
+
+        System.out.println("№4");
+        mass.massiv4();
+
+        System.out.println("№5");
+        mass.massiv5();
+
+    }}
